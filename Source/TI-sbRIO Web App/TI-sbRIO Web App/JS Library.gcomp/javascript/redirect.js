6e09c8ad0b0c0729f90a603bb4e5b297c7ae6614
titetil <gtetil@us.tiauto.com>
2019-11-12 11:18:01 -0500

    - First commit after upgrading to LV 2019. - Last change was adding functionality for Hella FCA controller.

---
+++ Source/TI-sbRIO Web App/TI-sbRIO Web App/JS Library.gcomp/javascript/redirect.js
@@ -0,0 +1,7 @@
+(function () {
+    // Use strict prevents silent and common JavaScript errors.
+    'use strict';
+    window.Redirect = function (URL) {
+        document.location.href=URL;
+    };
+}());
